729ffc1a80c240d90a23a240f0a4e5c0fcd1f825
yoosangyeob <syyoo@joara.com>
2022-08-13 16:53:45 +0900

    Define DAO

---
+++ app/src/main/java/com/example/busschedule/database/schdule/ScheduleDao.kt
@@ -0,0 +1,14 @@
+package com.example.busschedule.database.schdule
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.busschedule.database.Schedule
+
+@Dao
+interface ScheduleDao {
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): List<Schedule>
+
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): List<Schedule>
+}
